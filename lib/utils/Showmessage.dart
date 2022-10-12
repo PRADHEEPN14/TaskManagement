81791908a795988fe7873309b5fb42a1d5e0493e
PRADHEEPN14 <pradheep.n@skeintech.com>
2022-10-12 16:00:15 +0530

    snackbar fucntion

---
+++ lib/utils/Showmessage.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/material.dart';
+// class CustomWidgets {
+//  CustomWidgets._();
+//  static buildSnackbar(BuildContext context, String message) {
+//   Scaffold.of(context).showSnackBar(
+//     SnackBar(
+//       content: Text("$message"),
+//     ),
+//   );
+//  }
+// }
