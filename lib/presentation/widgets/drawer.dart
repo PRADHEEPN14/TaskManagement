6f140860508ef5d03fd3c6855f3371fe7948e9b8
PRADHEEPN14 <pradheep.n@skeintech.com>
2022-10-12 15:55:04 +0530

    commit

--- lib/presentation/widgets/drawer.dart
+++ lib/presentation/widgets/drawer.dart
@@ -92,7 +92,8 @@ class MyDrawer extends StatelessWidget {
                                     )
                                     ),
                               ],
-                            ));
+                            )
+                            );
                   },
                 ),
               ),
